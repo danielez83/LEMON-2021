bf9fb84efbd9a1936c2455720f96440ddf7de9dd
Daniele Zannoni <daniele@c02cx6s2ml85.klientdrift.uib.no>
2021-10-06 14:15:01 +0200

    Changes in fitting functions and interval for d18O

--- load_humidity_isotope_responses.py
+++ load_humidity_isotope_responses.py
@@ -20,24 +20,27 @@ from FARLAB_standards import standard
 d18O_standard   = standard('FINSE', 'd18O')
 dD_standard     = standard('FINSE', 'dD')
 
-filename_to_save = "FINSE_20210921.pkl"
+filename_to_save = "BER_20210921_extended_2.pkl"
 
 # --------------- BERMUDA 19 Sept
-#date_start      = datetime.datetime(year=2021,month=9,day=19,hour=10,minute=20)
-#date_stop       = datetime.datetime(year=2021,month=9,day=19,hour=13,minute=37)
+date_start      = datetime.datetime(year=2021,month=9,day=19,hour=10,minute=20)
+date_stop       = datetime.datetime(year=2021,month=9,day=19,hour=13,minute=37)
 # --------------- GLW 19 Sept
 #date_start      = datetime.datetime(year=2021,month=9,day=19,hour=14,minute=10)
-#date_stop       = datetime.datetime(year=2021,month=9,day=19,hour=16,minute=0)
+#date_stop       = datetime.datetime(year=2021,month=9,day=19,hour=16,minute=5)
 # --------------- GLW 20 Sept
 #date_start      = datetime.datetime(year=2021,month=9,day=20,hour=12,minute=16)
 #date_stop       = datetime.datetime(year=2021,month=9,day=20,hour=14,minute=45)
 # --------------- FINSE 21 Sept
-date_start      = datetime.datetime(year=2021,month=9,day=21,hour=9,minute=17)
-date_stop       = datetime.datetime(year=2021,month=9,day=21,hour=13,minute=30)
+#date_start      = datetime.datetime(year=2021,month=9,day=21,hour=9,minute=17)
+#date_stop       = datetime.datetime(year=2021,month=9,day=21,hour=13,minute=31)
 #%% Import NetCDF data
-# file2read = nc.Dataset('../Picarro_HIDS2254/2021/09/HIDS2254-20210919-DataLog_User.nc')
-# file2read = nc.Dataset('../Picarro_HIDS2254/2021/09/HIDS2254-20210920-DataLog_User (1).nc')
-file2read = nc.Dataset('../Picarro_HIDS2254/2021/09/HIDS2254-20210921-DataLog_User.nc')
+# Ok for BERMUDA and GLW 19.09.2021
+file2read = nc.Dataset('../Picarro_HIDS2254/2021/09/HIDS2254-20210919-DataLog_User.nc')
+# Ok for GLW 20.09.2021
+#file2read = nc.Dataset('../Picarro_HIDS2254/2021/09/HIDS2254-20210920-DataLog_User (1).nc')
+# Ok for FINSE
+#file2read = nc.Dataset('../Picarro_HIDS2254/2021/09/HIDS2254-20210921-DataLog_User.nc')
 #print(file2read)
 for dim in file2read.dimensions.values():
     print(dim)
@@ -62,26 +65,80 @@ Picarro_data = pd.DataFrame(data = d)
 Picarro_data.index = Picarro_data['Time']
 Picarro_data = Picarro_data.drop(columns = 'Time')
 
-#%% Subset data by dates BERMUDA
+#%% Subset data by dates for standard of interest
 good_indexes    = [Picarro_data.index > date_start, Picarro_data.index < date_stop] # Select by dates
 good_indexes    = good_indexes[0] & good_indexes[1]                                 # Make an array of logicals
 
 Picarro_data_subset = Picarro_data[good_indexes]
+#%% Clean data
+# BER 19.09.2021
+bad_date_start = datetime.datetime(year=2021,month=9,day=19,hour=12,minute=10)
+bad_date_stop = datetime.datetime(year=2021,month=9,day=19,hour=12,minute=16)
+bad_indexes = [Picarro_data_subset.index > bad_date_start, Picarro_data_subset.index < bad_date_stop] # Select by dates
+bad_indexes = bad_indexes[0] & bad_indexes[1]
+Picarro_data_subset.d18O[bad_indexes] = np.nan
+Picarro_data_subset.dD[bad_indexes] = np.nan
+# ------------------------------------------------------------
+# GLW 19.09.2021: remove spike between 14:52 - 14:55 -------
+bad_date_start = datetime.datetime(year=2021,month=9,day=19,hour=14,minute=52)
+bad_date_stop = datetime.datetime(year=2021,month=9,day=19,hour=14,minute=55)
+bad_indexes = [Picarro_data_subset.index > bad_date_start, Picarro_data_subset.index < bad_date_stop] # Select by dates
+bad_indexes = bad_indexes[0] & bad_indexes[1]
+Picarro_data_subset.d18O[bad_indexes] = np.nan
+Picarro_data_subset.dD[bad_indexes] = np.nan
+# ------------------------------------------------------------
+# GLW 20.09.2021: remove spike between 14:15 - 14:20 -------
+bad_date_start = datetime.datetime(year=2021,month=9,day=20,hour=14,minute=15)
+bad_date_stop = datetime.datetime(year=2021,month=9,day=20,hour=14,minute=21)
+bad_indexes = [Picarro_data_subset.index > bad_date_start, Picarro_data_subset.index < bad_date_stop] # Select by dates
+bad_indexes = bad_indexes[0] & bad_indexes[1]
+Picarro_data_subset.H2O[bad_indexes] = np.nan
+Picarro_data_subset.d18O[bad_indexes] = np.nan
+Picarro_data_subset.dD[bad_indexes] = np.nan
+# ------------------------------------------------------------
+# ------------------------------------------------------------
+# FINSE 21.09.2021: remove spikes
+bad_date_start = datetime.datetime(year=2021,month=9,day=21,hour=10,minute=14)
+bad_date_stop = datetime.datetime(year=2021,month=9,day=21,hour=10,minute=19)
+bad_indexes1 = [Picarro_data_subset.index > bad_date_start, Picarro_data_subset.index < bad_date_stop] # Select by dates
+bad_indexes1 = bad_indexes1[0] & bad_indexes1[1]
+
+bad_date_start = datetime.datetime(year=2021,month=9,day=21,hour=10,minute=46)
+bad_date_stop = datetime.datetime(year=2021,month=9,day=21,hour=10,minute=49)
+bad_indexes2 = [Picarro_data_subset.index > bad_date_start, Picarro_data_subset.index < bad_date_stop] # Select by dates
+bad_indexes2 = bad_indexes2[0] & bad_indexes2[1]
+
+bad_date_start = datetime.datetime(year=2021,month=9,day=21,hour=11,minute=35)
+bad_date_stop = datetime.datetime(year=2021,month=9,day=21,hour=11,minute=45)
+bad_indexes3 = [Picarro_data_subset.index > bad_date_start, Picarro_data_subset.index < bad_date_stop] # Select by dates
+bad_indexes3 = bad_indexes3[0] & bad_indexes3[1]
+
+bad_date_start = datetime.datetime(year=2021,month=9,day=21,hour=12,minute=50)
+bad_date_stop = datetime.datetime(year=2021,month=9,day=21,hour=12,minute=58)
+bad_indexes4 = [Picarro_data_subset.index > bad_date_start, Picarro_data_subset.index < bad_date_stop] # Select by dates
+bad_indexes4 = bad_indexes4[0] & bad_indexes4[1]
+
+bad_indexes = bad_indexes1 | bad_indexes2 | bad_indexes3 | bad_indexes4
+
+Picarro_data_subset.H2O[bad_indexes] = np.nan
+Picarro_data_subset.d18O[bad_indexes] = np.nan
+Picarro_data_subset.dD[bad_indexes] = np.nan
+# ------------------------------------------------------------
 #%% Plot H2O timeseries
 fig, ax = plt.subplots(nrows = 3, figsize = (10, 8))
 # date_format = mdates.DateFormatter("%Y-%m-%d %H:%M:%S")
 date_format = mdates.DateFormatter("%H:%M:%S")
-ax[0].plot(Picarro_data.index[good_indexes], Picarro_data.H2O[good_indexes])
+ax[0].plot(Picarro_data_subset.index, Picarro_data_subset.H2O)
 ax[0].xaxis.set_major_formatter(date_format)
 ax[0].set(ylabel = 'H$_{2}$O [ppm]')
 ax[0].grid()
 
-ax[1].plot(Picarro_data.index[good_indexes], Picarro_data.d18O[good_indexes])
+ax[1].plot(Picarro_data_subset.index, Picarro_data_subset.d18O)
 ax[1].xaxis.set_major_formatter(date_format)
 ax[1].set(ylabel = '$\delta^{18}$O [‰]')
 ax[1].grid()
 
-ax[2].plot(Picarro_data.index[good_indexes], Picarro_data.dD[good_indexes])
+ax[2].plot(Picarro_data_subset.index, Picarro_data_subset.dD)
 ax[2].xaxis.set_major_formatter(date_format)
 ax[2].set(ylabel = '$\delta$D [‰]')
 ax[2].grid()
@@ -90,27 +147,28 @@ ax[2].grid()
 
 #%% Bin data based on H2O levels
 #levels          = np.histogram_bin_edges(Picarro_data_subset['H2O'], bins = 20)
-levels          = np.histogram_bin_edges(Picarro_data_subset['H2O'])
+# levels          = np.histogram_bin_edges(Picarro_data_subset['H2O'])
+levels = np.histogram_bin_edges(Picarro_data_subset.H2O[~np.isnan(Picarro_data_subset.H2O)])
 level_window    = 300
 
 # Preallocate memory for level values
-H2O_level   = np.zeros((len(levels)-1, 1))
-d18O_means  = np.zeros((len(levels)-1, 1))
-d18O_stds   = np.zeros((len(levels)-1, 1))
-dD_means    = np.zeros((len(levels)-1, 1))
-dD_stds     = np.zeros((len(levels)-1, 1))
+H2O_level   = np.zeros((len(levels), 1))
+d18O_means  = np.zeros((len(levels), 1))
+d18O_stds   = np.zeros((len(levels), 1))
+dD_means    = np.zeros((len(levels), 1))
+dD_stds     = np.zeros((len(levels), 1))
 idx = 0
 
-for level in levels[1:]:
+for level in levels[:]:
     good_indexes    = [Picarro_data_subset['H2O'] > (level - level_window), Picarro_data_subset['H2O'] < (level + level_window)]
     good_indexes    = good_indexes[0] & good_indexes[1] # Make an array of logicals
     # ---
-    H2O_level[idx]  = Picarro_data_subset.H2O[good_indexes].mean()
-    d18O_means[idx] = Picarro_data_subset.d18O[good_indexes].mean()
-    dD_means[idx]   = Picarro_data_subset.dD[good_indexes].mean()
+    H2O_level[idx]  = Picarro_data_subset.H2O[good_indexes].mean(skipna=True)
+    d18O_means[idx] = Picarro_data_subset.d18O[good_indexes].mean(skipna=True)
+    dD_means[idx]   = Picarro_data_subset.dD[good_indexes].mean(skipna=True)
     # ---
-    d18O_stds[idx] = Picarro_data_subset.d18O[good_indexes].std()
-    dD_stds[idx]   = Picarro_data_subset.dD[good_indexes].std()
+    d18O_stds[idx] = Picarro_data_subset.d18O[good_indexes].std(skipna=True)
+    dD_stds[idx]   = Picarro_data_subset.dD[good_indexes].std(skipna=True)
     # ---
     idx+=1
 # Prepare numpy array
@@ -118,6 +176,7 @@ d = np.concatenate((H2O_level, d18O_means, d18O_stds, dD_means, dD_stds), axis =
 
 #%% Save DataFrame
 my_data_frame = pd.DataFrame(d, columns = ['H2O', 'd18O_mean', 'd18O_err', 'dD_mean', 'dD_err'])
+my_data_frame.dropna(inplace = True)
 my_data_frame.to_pickle(filename_to_save)
 #%% Plot curves
 fig, ax = plt.subplots(nrows = 2, figsize = [15,10])
