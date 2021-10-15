fdb367124e609e84fcc229a06585086406d25490
Daniele Zannoni <daniele@c02cx6s2ml85.klientdrift.uib.no>
2021-10-15 16:56:21 +0200

    Update on plot scripts

--- load_calibration_Picarro.py
+++ load_calibration_Picarro.py
@@ -38,39 +38,51 @@ calibration_data['dD'] = hum_corr_fun(calibration_data['H20'], calibration_data[
 
 # %% Plot section for Bermuda standard
 # Plot calibration trend for 
-fig, ax = plt.subplots(nrows = 2, figsize = [15,10])
+fig, ax = plt.subplots(nrows = 3, figsize = [15,10])
 
-calibration_data['d18O'][calibration_data['Standard'] == 'BERM'].plot(ax = ax[0])
-ax[0].plot([np.min(calibration_data.index), np.max(calibration_data.index)], [0.5750, 0.5750],
-           color = [1,0,0], linestyle = '--')
+calibration_data['d18O'][calibration_data['Standard'] == 'BER'].plot(ax = ax[0])
+#ax[0].plot([np.min(calibration_data.index), np.max(calibration_data.index)], [0.5750, 0.5750],
+#           color = [1,0,0], linestyle = '--')
 ax[0].grid()
-ax[0].set(ylabel = "$\delta^{18}$O [‰]", ylim = [-1, 1])
-ax[0].text(np.min(calibration_data.index), 0.75, "True Bermuda $\delta^{18}$O value")
+ax[0].set(ylabel = "$\delta^{18}$O [‰]", ylim = [-1, 0.5])
+#ax[0].text(np.min(calibration_data.index), 0.75, "True Bermuda $\delta^{18}$O value")
 
-calibration_data['dD'][calibration_data['Standard'] == 'BERM'].plot(ax = ax[1])
-ax[1].plot([np.min(calibration_data.index), np.max(calibration_data.index)], [6.47, 6.47],
-           color = [1,0,0], linestyle = '--')
+calibration_data['dD'][calibration_data['Standard'] == 'BER'].plot(ax = ax[1])
+#ax[1].plot([np.min(calibration_data.index), np.max(calibration_data.index)], [6.47, 6.47],
+#           color = [1,0,0], linestyle = '--')
 ax[1].grid()
-ax[1].set(ylabel = "$\delta$D [‰]", ylim = [0, 20])
-ax[1].text(np.min(calibration_data.index), 7.5, "True Bermuda $\delta$D value")
+ax[1].set(ylabel = "$\delta$D [‰]", ylim = [12, 20])
+#ax[1].text(np.min(calibration_data.index), 7.5, "True Bermuda $\delta$D value")
+
+calibration_data['H20'][calibration_data['Standard'] == 'BER'].plot(ax = ax[2])
+#ax[1].plot([np.min(calibration_data.index), np.max(calibration_data.index)], [-308.14, -308.14],
+#           color = [1,0,0], linestyle = '--')
+ax[2].grid()
+ax[2].set(ylabel = "H$_2$O [ppm]", ylim = [15000, 23000])
 
 # %% Plot section for GLW standard
 # Plot calibration trend for 
-fig, ax = plt.subplots(nrows = 2, figsize = [15,10])
+fig, ax = plt.subplots(nrows = 3, figsize = [15,10])
 
 calibration_data['d18O'][calibration_data['Standard'] == 'GLW'].plot(ax = ax[0])
-ax[0].plot([np.min(calibration_data.index), np.max(calibration_data.index)], [-40.0608, -40.0608],
-           color = [1,0,0], linestyle = '--')
+#ax[0].plot([np.min(calibration_data.index), np.max(calibration_data.index)], [-40.0608, -40.0608],
+#           color = [1,0,0], linestyle = '--')
 ax[0].grid()
-ax[0].set(ylabel = "$\delta^{18}$O [‰]", ylim = [-41, -35])
-ax[0].text(np.min(calibration_data.index), -39.5, "True Bermuda $\delta^{18}$O value")
+ax[0].set(ylabel = "$\delta^{18}$O [‰]", ylim = [-37, -35.5])
+#ax[0].text(np.min(calibration_data.index), -39.5, "True Bermuda $\delta^{18}$O value")
 
 calibration_data['dD'][calibration_data['Standard'] == 'GLW'].plot(ax = ax[1])
-ax[1].plot([np.min(calibration_data.index), np.max(calibration_data.index)], [-308.14, -308.14],
-           color = [1,0,0], linestyle = '--')
+#ax[1].plot([np.min(calibration_data.index), np.max(calibration_data.index)], [-308.14, -308.14],
+#           color = [1,0,0], linestyle = '--')
 ax[1].grid()
-ax[1].set(ylabel = "$\delta$D [‰]", ylim = [-330, -300])
-ax[1].text(np.min(calibration_data.index), -306, "True Bermuda $\delta$D value")
+ax[1].set(ylabel = "$\delta$D [‰]", ylim = [-328, -322])
+#ax[1].text(np.min(calibration_data.index), -306, "True Bermuda $\delta$D value")
+
+calibration_data['H20'][calibration_data['Standard'] == 'GLW'].plot(ax = ax[2])
+#ax[1].plot([np.min(calibration_data.index), np.max(calibration_data.index)], [-308.14, -308.14],
+#           color = [1,0,0], linestyle = '--')
+ax[2].grid()
+ax[2].set(ylabel = "H$_2$O [ppm]", ylim = [15000, 23000])
 
 #%% Calculate calibration factors
 # Days in standard table
@@ -124,7 +136,7 @@ df = pd.DataFrame(data = d, index = d['Date'])
 df.drop(columns = 'Date', inplace = True)
 df.to_pickle(filename_to_save)
 
-#%% Plot calibration parameters
+#%% Plot calibration parameters - SLOPE
 fig, ax1 = plt.subplots(figsize = (11,10))
 ax2 = ax1.twinx()
 
@@ -140,3 +152,18 @@ ax2.tick_params(axis='both', which='major', labelsize=16, colors='red')
 
 ax1.grid()
 
+#%% Plot calibration parameters - Intercept
+fig, ax1 = plt.subplots(figsize = (11,10))
+ax2 = ax1.twinx()
+
+ax1.plot(df.index, df['Intercept_d18O'], 'k-')
+ax2.plot(df.index, df['Intercept_dD'], 'r--')
+
+ax1.set_ylabel('Intercept $\delta^{18}$O [‰]', fontsize=24)
+ax1.tick_params(axis='both', which='major', labelsize=16)
+
+ax2.set_ylabel('Intercept $\delta$D [‰]', fontsize=24, color = 'red')
+ax2.tick_params(axis='both', which='major', labelsize=16, colors='red')
+#ax2.set_yticks(np.linspace(0.914, 0.928, 9))
+
+ax1.grid()
--- plot_picarro_status.py
+++ plot_picarro_status.py
@@ -12,14 +12,15 @@ import matplotlib.pyplot as plt
 import datetime
 import netCDF4 as nc
 #from scipy.stats import linregress
+import matplotlib.dates as mdates
 
 from hum_corr_fun import hum_corr_fun
 
 #%% Configuration
 #iMet_filename      = '../iMet/44508/iMet-XQ2-44508_20210918.nc'
-Picarro_filename   = '../Picarro_HIDS2254/2021/09/HIDS2254-20210917-DataLog_User.nc'
+Picarro_filename   = '../Picarro_HIDS2254/2021/09/HIDS2254-20210923-DataLog_User.nc'
 Flight_table_filename = '../Excel/Flights_Table.csv'
-Flight_OI =  3#[4,5,6,7]#[8]#[9,10,11]#[12]#[14,15]#[16]
+Flight_OI =  16#[4,5,6,7]#[8]#[9,10,11]#[12,13]#[14,15]#[16]
 Calibration_param_filename = 'Standard_reg_param.pkl'
 
 #%% Pressure to alitude conversion
@@ -80,22 +81,118 @@ df_Picarro_subset = df_Picarro.loc[df_Picarro_mask]
 start_date = df_Flight_table['Takeoff_fulldate'][df_Flight_table['Flight ID'] == Flight_OI]
 start_date = start_date - datetime.timedelta(minutes = 50)
 stop_date = df_Flight_table['Landing_fulldate'][df_Flight_table['Flight ID'] == Flight_OI]
-stop_date = stop_date - datetime.timedelta(minutes = 5)
+stop_date = stop_date - datetime.timedelta(minutes = 81)
 df_Picarro_before_mask = (df_Picarro.index > start_date.iloc[0]) & (df_Picarro.index < stop_date.iloc[0])
 df_Picarro_before_subset = df_Picarro.loc[df_Picarro_before_mask]
 
 # After Flight 45 min
 start_date = df_Flight_table['Takeoff_fulldate'][df_Flight_table['Flight ID'] == Flight_OI]
-start_date = start_date + datetime.timedelta(minutes = 5)
+start_date = start_date + datetime.timedelta(minutes = 81)
 stop_date = df_Flight_table['Landing_fulldate'][df_Flight_table['Flight ID'] == Flight_OI]
 stop_date = stop_date + datetime.timedelta(minutes = 50)
 df_Picarro_after_mask = (df_Picarro.index > start_date.iloc[0]) & (df_Picarro.index < stop_date.iloc[0])
 df_Picarro_after_subset = df_Picarro.loc[df_Picarro_after_mask]
 
 #%% PLot timeseries
+date_format = mdates.DateFormatter("%H:%M")
 
-fig, ((ax1, ax2, ax3), (ax4, ax5, ax6), (ax7, ax8, ax9)) = plt.subplots(nrows = 3, ncols=3, figsize=(15,10))
-#plt.subplots_adjust(wspace = .05) 
-ax1.scatter(df_Picarro_subset['d18O'], df_Picarro_subset['d18O'])
-ax5.scatter(df_Picarro_subset['d18O'], df_Picarro_subset['d18O'])
-ax9.scatter(df_Picarro_subset['d18O'], df_Picarro_subset['d18O'])
+fig, ((ax1, ax2, ax3), (ax4, ax5, ax6), (ax7, ax8, ax9), (ax10, ax11, ax12)) = plt.subplots(nrows = 4, ncols=3, figsize=(15,10))
+plt.subplots_adjust(wspace = .05) 
+
+# Cavity Temperature ---------------------------------------------------------
+CavityTemp_ylim = [round(df_Picarro_subset['CavityTemp'].min(), ndigits=2),
+                   round(df_Picarro_after_subset['CavityTemp'].max(), ndigits=2)]
+# before
+ax1.plot(df_Picarro_before_subset.index, df_Picarro_before_subset['CavityTemp'], 'k-')
+ax1.set(ylim=CavityTemp_ylim, ylabel = "Cavity Temperature [˚C]")
+ax1.xaxis.set_major_formatter(date_format)
+ax1.grid()
+buff_text = ("Before flight %d" % Flight_OI)
+ax1.set_title(buff_text)
+# during
+ax2.plot(df_Picarro_subset.index, df_Picarro_subset['CavityTemp'], 'r-')
+ax2.set(ylim=CavityTemp_ylim)
+ax2.yaxis.set_ticklabels([])
+ax2.xaxis.set_major_formatter(date_format)
+ax2.grid()
+buff_text = ("During flight %d" % Flight_OI)
+ax2.set_title(buff_text)
+# after 
+ax3.plot(df_Picarro_after_subset.index, df_Picarro_after_subset['CavityTemp'], 'k-')
+ax3.set(ylim=CavityTemp_ylim)
+ax3.yaxis.set_ticklabels([])
+ax3.xaxis.set_major_formatter(date_format)
+ax3.grid()
+buff_text = ("After flight %d" % Flight_OI)
+ax3.set_title(buff_text)
+
+# Cavity Pressure ---------------------------------------------------------
+CavityPress_ylim = [round(df_Picarro_subset['CavityPressure'].min(), ndigits=2), 
+                    round(df_Picarro_subset['CavityPressure'].max(), ndigits=2)]
+# before
+ax4.plot(df_Picarro_before_subset.index, df_Picarro_before_subset['CavityPressure'], 'k-')
+ax4.set(ylim=CavityPress_ylim, ylabel = "Cavity Pressure [Torr]")
+ax4.xaxis.set_major_formatter(date_format)
+ax4.grid()
+# during
+ax5.plot(df_Picarro_subset.index, df_Picarro_subset['CavityPressure'], 'r-')
+ax5.set(ylim=CavityPress_ylim)
+ax5.yaxis.set_ticklabels([])
+ax5.xaxis.set_major_formatter(date_format)
+ax5.grid()
+# after 
+ax6.plot(df_Picarro_after_subset.index, df_Picarro_after_subset['CavityPressure'], 'k-')
+ax6.set(ylim=CavityPress_ylim)
+ax6.yaxis.set_ticklabels([])
+ax6.xaxis.set_major_formatter(date_format)
+ax6.grid()
+
+# Warm Box Temperature ---------------------------------------------------------
+WarmBox_ylim = [round(df_Picarro_subset['WarmBoxTemp'].min(), ndigits=2),
+                round(df_Picarro_subset['WarmBoxTemp'].max()+.1, ndigits=2)]
+# before
+ax7.plot(df_Picarro_before_subset.index, df_Picarro_before_subset['WarmBoxTemp'], 'k-')
+ax7.set(ylim=WarmBox_ylim, ylabel = "Warm Box Temperature [˚C]")
+ax7.xaxis.set_major_formatter(date_format)
+ax7.grid()
+# during
+ax8.plot(df_Picarro_subset.index, df_Picarro_subset['WarmBoxTemp'], 'r-')
+ax8.set(ylim=WarmBox_ylim)
+ax8.yaxis.set_ticklabels([])
+ax8.xaxis.set_major_formatter(date_format)
+ax8.grid()
+# after 
+ax9.plot(df_Picarro_after_subset.index, df_Picarro_after_subset['WarmBoxTemp'], 'k-')
+ax9.set(ylim=WarmBox_ylim)
+ax9.yaxis.set_ticklabels([])
+ax9.xaxis.set_major_formatter(date_format)
+ax9.grid()
+
+# DAS Temperature ---------------------------------------------------------
+DAS_ylim = [round(df_Picarro_subset['DasTemp'].min()-5, ndigits=2),
+            round(df_Picarro_after_subset['DasTemp'].max()+5, ndigits=2)]
+# before
+ax10.plot(df_Picarro_before_subset.index, df_Picarro_before_subset['DasTemp'], 'k-')
+ax10.set(ylim=DAS_ylim, ylabel = "DAS Temperature [˚C]")
+ax10.xaxis.set_major_formatter(date_format)
+ax10.grid()
+# during
+ax11.plot(df_Picarro_subset.index, df_Picarro_subset['DasTemp'], 'r-')
+ax11.set(ylim=DAS_ylim)
+ax11.yaxis.set_ticklabels([])
+ax11.xaxis.set_major_formatter(date_format)
+ax11.grid()
+# after 
+ax12.plot(df_Picarro_after_subset.index, df_Picarro_after_subset['DasTemp'], 'k-')
+ax12.set(ylim=DAS_ylim)
+ax12.yaxis.set_ticklabels([])
+ax12.xaxis.set_major_formatter(date_format)
+ax12.grid()
+
+#ax7.plot(df_Picarro_before_subset.index, df_Picarro_before_subset['CavityTemp'], 'k-')
+#ax7.set(ylim=[79.5, 80.5])
+#ax7.xaxis.set_major_formatter(date_format)
+fig.autofmt_xdate()
+
+buff_text = ("%d-%d-%d" % (df_Picarro_subset.index[0].day, df_Picarro_subset.index[0].month, df_Picarro_subset.index[0].year))
+fig.suptitle(buff_text, fontsize=16)
