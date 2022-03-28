2497899202ece09a6f9d69d37ba038d1357700ef
Daniele Zannoni <daniele@c02cx6s2ml85.klientdrift.uib.no>
2022-03-28 17:48:07 +0200

    Moran I first calculation, seems ok. Need to be tested

--- distance_function.py
+++ distance_function.py
@@ -19,7 +19,7 @@ from scipy import signal
 from pyproj import Proj
 
 #%% Configuration
-data_filename            = '../PKL final data/flight_04.pkl'
+data_filename            = '../PKL final data/flight_07.pkl'
 
 
 display                     = 'raw' #'raw', 'binned'
@@ -30,6 +30,8 @@ bins                        = np.arange(400, 20000, 50)
 #%% Load data
 with open(data_filename, 'rb') as f:
     flight_data, metadata = pickle.load(f)
+
+flight_data = flight_data[flight_data['ALT'] < 2000]
     
 #%% Convert Lat Lon in UTM (meters)
 # https://ocefpaf.github.io/python4oceanographers/blog/2013/12/16/utm/
@@ -38,17 +40,64 @@ UTMx, UTMy = myProj(flight_data['LON'].to_numpy(), flight_data['LAT'].to_numpy()
 flight_data['UTMx'] = UTMx
 flight_data['UTMy'] = UTMy
 
-pos = [flight_data['UTMx'].to_numpy() - flight_data['UTMx'].mean(),
-       flight_data['UTMy'].to_numpy() - flight_data['UTMy'].mean(),
-       flight_data['ALT'].to_numpy() - flight_data['ALT'].mean(),]
+pos = np.array([flight_data['UTMx'].to_numpy() - flight_data['UTMx'].mean(),
+                flight_data['UTMy'].to_numpy() - flight_data['UTMy'].mean(),
+                flight_data['ALT'].to_numpy() - flight_data['ALT'].mean(),])
+pos = pos.T
+data = flight_data['dexcess'].to_numpy()
 
+#%%
+def distance_matrix(pos):
+    """
+    Parameters
+    ----------
+    pos : numpy array of location in space
+          THe dimension of the pos array must be
+          elements on the rows (n), dimensions (m) on the comlumn. E.g.
+          pos = numpy.array([x0, y0, z0], .. , [xn, yn, zn])
+
+    Returns
+    -------
+    The euclidean distances on a n by n matrix.
+
+    """
+    import numpy as np
+    # Number of elements in array
+    elements = np.size(pos, 0)
+    # Dimension of array (eg 2D or 3D)
+    dim = np.size(pos, 1)
+    dist_array = np.zeros((elements, elements))
+    for e in range(elements):
+        dist = 0
+        for i in range(dim):
+            dist += (np.repeat(pos[e,i], elements) - pos[:,i])**2
+        dist = np.sqrt(dist)
+        dist_array[:,e] = dist
+    return dist_array
 #%% Calculate distances
+# Array with dimension >= 2
+# Rows = elements
+# Columns = dimensions
+# Return the matrix of distances based on euclidean distance
 
-import numpy as np
-# Dimension of array (eg 2D or 3D)
-dim = np.size(pos, 0)
-elements = np.size(pos, 1)
-dist = 0
-for e in range(elements):
-    for i in range(dim):
-        dist += pos[:,i]
+# Compute delta matrix
+dist_matrix = distance_matrix(pos)
+prod = 0
+denom = 0
+S_0 = 0
+for i in range (np.size(dist_matrix, 0)):
+    position_of_interest = np.where(dist_matrix[:,i] == 0)[0][0]
+    other_positions = np.arange(position_of_interest + 1, np.size(dist_matrix, 0))
+    z_i = data[position_of_interest] - data.mean()
+    denom += z_i**2 
+    for j in other_positions:
+        z_j = data[j] - data.mean()
+        w_i_j = 1/dist_matrix[j, position_of_interest]
+        prod += w_i_j * z_i * z_j
+        S_0 += w_i_j
+ratio = prod/denom
+#%%
+n = len(data)
+MoranI = (n/S_0) * ratio
+
+print(MoranI)
